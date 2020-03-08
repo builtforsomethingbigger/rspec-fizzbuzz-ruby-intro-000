9f4043c3b4eae0a6aee322f9407724be093271ff
Steve C <stephen.cases@gmail.com>
2020-03-08 04:52:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && !(int % 5 == 0)
-    "FizzBuzz"
+    "Fizz"
   elsif int % 5 == 0 && !(int % 3 == 0)
     "Buzz"
   elsif int % 15 == 0
-    "Fizz"
+    "FizzBuzz"
   else
     nil
   end
